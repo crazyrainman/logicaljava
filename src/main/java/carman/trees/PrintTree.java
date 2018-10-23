17a660c4ba81fe65713a46ec60fb4ff02d566c6a
Eric Gao <Eric.Gao@bentley.com>
2018-10-23 11:23:08 +0800

    print tree

--- src/main/java/carman/trees/PrintTree.java
+++ src/main/java/carman/trees/PrintTree.java
@@ -1,11 +1,16 @@
 package carman.trees;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class PrintTree {
     public void printTree(Node head) {
         System.out.println("Binary Tree: ");
         printInOrder(head, 0, "H", 17);
         System.out.println();
     }
+
     public void printInOrder(Node head, int height, String to, int len) {
         if (head == null) {
             return;
@@ -19,6 +24,7 @@ public class PrintTree {
         System.out.println(getSpace(height * len) + val);
         printInOrder(head.left, height + 1, "^", len);
     }
+
     public String getSpace(int num) {
         String space = " ";
         StringBuffer buf = new StringBuffer("");
@@ -33,5 +39,84 @@ public class PrintTree {
         Node head = new SerialDeSerial().reconByPreString(preStr);
         new PrintTree().printTree(head);
         System.out.println(new SerialDeSerial().serialByPre(head));
+    }
+    void printByLevel(Node head) {
+        if (head == null) {
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        int level = 1;
+        Node last = head; // head 队头 
+        Node nLast = null; 
+        queue.offer(head); //  {1}
+        System.out.print("Level " + (level++) + " : ");
+        while(!queue.isEmpty()) {
+            head = queue.poll(); 
+            System.out.print(head.value + " ");
+            if (head.left != null) {
+                queue.offer(head.left); 
+                nLast = head.left;
+            }
+            if (head.right != null) {
+                queue.offer(head.right);
+                nLast = head.right;
+            } // 
+            if (head == last && !queue.isEmpty()) { 
+                System.out.print("\nLevel " + (level++) + " : ");
+                last = nLast;
+            }
+        }
+        System.out.println();
+    }
+
+    void printByZigZig(Node head) {
+        if (head == null) {
+            return;
+        }
+        
+        Deque<Node> dq = new LinkedList<>();
+        int level = 1; 
+        boolean lr = true;
+        Node last = head; 
+        Node nLast = null; 
+        dq.offerFirst(head); 
+
+        printLevelAndOrientation(level++, lr);
+        while (!dq.isEmpty()) {
+            if (lr) { 
+                head = dq.pollFirst();  
+                if (head.left != null) {
+                    nLast = nLast == null ? head.left : nLast;
+                    dq.offerLast(head.left);
+                }
+                if (head.right != null) {
+                    nLast = nLast == null ? head.right : nLast;
+                    dq.offerLast(head.right);
+                }
+            } else {
+                head = dq.pollLast();
+                if (head.right != null) {
+                    nLast = nLast == null ? head.right : nLast;
+                    dq.offerFirst(head.right);
+                }
+                if (head.left != null) {
+                    nLast = nLast == null ? head.left : nLast;
+                    dq.offerFirst(head.left);
+                }
+            }
+            System.out.print(head.value + " ");
+            if (head == last && !dq.isEmpty()) {
+                lr = !lr;
+                last = nLast;
+                System.out.println();
+                printLevelAndOrientation(level++, lr);
+            }
+        }
+        System.out.println();
+    }
+
+    void printLevelAndOrientation(int level, boolean lr) {
+        System.out.print("Level " + level + " from ");
+        System.out.print(lr ? "left to right: " : "right to left: ");
     }
 }
